72348c4b0be11a80c4aceec3e162f8239292ba2b
hasinduudara <hasindutechschool@gmail.com>
2024-11-20 12:37:14 +0530

    Show username, name, password

--- src/main/java/lk/ijse/gdse/staysmartproject/controller/ProfileEditePageController.java
+++ src/main/java/lk/ijse/gdse/staysmartproject/controller/ProfileEditePageController.java
@@ -7,6 +7,10 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import lk.ijse.gdse.staysmartproject.util.CrudUtil;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class ProfileEditePageController {
 
@@ -18,6 +22,9 @@ public class ProfileEditePageController {
 
     @FXML
     private Label lblEmail;
+
+    @FXML
+    private Label lblName;
 
     @FXML
     private Label lblUserName;
@@ -42,6 +49,23 @@ public class ProfileEditePageController {
     @FXML
     void btnProfimeImageUploadAction(ActionEvent event) {
 
+    }
+
+    public void initialize() {
+        loadUserProfile();
+    }
+
+    private void loadUserProfile() {
+        try {
+            ResultSet resultSet = CrudUtil.execute("SELECT Name, User_Name, Email FROM User WHERE User_ID = ?", "U001");
+            if (resultSet.next()) {
+                lblName.setText(resultSet.getString("Name"));
+                lblUserName.setText(resultSet.getString("User_Name"));
+                lblEmail.setText(resultSet.getString("Email"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
